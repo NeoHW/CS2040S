b078c52c1970771df3b075061587c4aeabe6d0dc
NeoHW <neohaowei@gmail.com>
2023-08-16 11:15:18 +0800

    shortening syntax

--- PS0/addingtrouble.java
+++ PS0/addingtrouble.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class addingtrouble {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        String ans = (num1 + num2 == num3) ? "correct!" : "wrong!";
-        System.out.println(ans);
+        int num1 = sc.nextInt(), num2 = sc.nextInt(), num3 = sc.nextInt();
+        //String ans = (num1 + num2 == num3) ? "correct!" : "wrong!";
+        // System.out.println(ans);
+        System.out.println((num1+num2==num3) ? "correct!" : "wrong!");
     }
 }

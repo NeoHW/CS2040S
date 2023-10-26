1c0785cbc516cda07c50114468e178c92ceadf9d
NeoHW <neohaowei@gmail.com>
2023-10-27 00:39:06 +0800

    PE prep

---
+++ PE/q1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class q1 {
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+
+        pw.flush();
+        pw.close();
+    }
+}

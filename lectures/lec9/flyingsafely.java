ba66097d9b68e04fede5bfdd1712ca748380254f
NeoHW <neohaowei@gmail.com>
2023-11-17 01:55:07 +0800

    not complete

---
+++ lectures/lec9/flyingsafely.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class flyingsafely {
+    public static void main (String[] args) {
+        // dfs algo?
+        Scanner sc = new Scanner(System.in);
+
+        int tc = sc.nextInt(); sc.nextLine();
+        while(tc-- >0) {
+            int n = sc.nextInt(); 
+            int m = sc.nextInt(); 
+            sc.nextLine();
+
+            while(m-- >0) {
+
+            }
+        }
+    }
+}
